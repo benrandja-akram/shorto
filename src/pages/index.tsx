f6ef82295c325f441fb7ac0244de2f1a7edc99bc
Benrandja Akram <a.benrandja@gmail.com>
2022-09-04 17:14:54 +0100

    init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="py-12 px-4">
+      <main className="max-w-5xl mx-auto px-4">
+        <h1 className="text-6xl font-extrabold tracking-tighter">
+          Collect, Deliver the best NFTs
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
