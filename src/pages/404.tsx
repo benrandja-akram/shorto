f5fca55b26e84f12b174340301e9ca85b6b91403
Benrandja Akram <a.benrandja@gmail.com>
2022-09-12 19:33:35 +0100

    host font locally

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,30 @@
-import type { NextPage } from 'next'
+import Link from 'next/link'
 
-const Home: NextPage = () => {
+function NotFound() {
   return (
-    <div className="py-12 px-4">
-      <main className="mx-auto max-w-5xl px-4">
-        <h1 className="text-6xl font-extrabold tracking-tighter">Not Found</h1>
-      </main>
+    <div className="flex h-screen w-screen flex-col space-y-10 px-4 pt-12 text-gray-900 md:items-center md:pt-48">
+      <div className="md: flex flex-col space-y-8 md:flex-row md:space-y-0 md:divide-x">
+        <div className="text-4xl font-bold tracking-tighter text-indigo-600 md:pr-8 md:text-6xl">
+          404
+        </div>
+        <div className="md:pl-8">
+          <h1 className="text-4xl font-extrabold tracking-tighter md:text-6xl">
+            Page not found
+          </h1>
+          <p className="mt-3 text-lg text-gray-900/70">
+            Please check the URL in the address bar and try again.
+          </p>
+        </div>
+      </div>
+      <Link href="/">
+        <a className="md:mr-32">
+          <button className="h-12 w-44 rounded bg-indigo-600 text-lg font-semibold text-white transition-colors hover:bg-indigo-500">
+            Go back home
+          </button>
+        </a>
+      </Link>
     </div>
   )
 }
 
-export default Home
+export default NotFound
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,16 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="crossorigin"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=Londrina+Outline&display=swap"
-          rel="stylesheet"
-        />
         <link rel="icon" type="image/svg" href="/logo.png" />
       </Head>
       <body>
