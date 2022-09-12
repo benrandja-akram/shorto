7963f5c3c14238f6dde0601b07c13ac6634d9a3e
Benrandja Akram <a.benrandja@gmail.com>
2022-09-12 19:45:54 +0100

    preload fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,49 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" type="image/svg" href="/logo.png" />
+
+        <link
+          rel="preload"
+          href="/fonts/londrina-outline-v23-latin-regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="crossorigin"
+        />
+        <link
+          rel="preload"
+          href="/fonts/inter-v12-latin-regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="crossorigin"
+        />
+        <link
+          rel="preload"
+          href=" /fonts/inter-v12-latin-500.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="crossorigin"
+        />
+        <link
+          rel="preload"
+          href=" /fonts/inter-v12-latin-600.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="crossorigin"
+        />
+        <link
+          rel="preload"
+          href=" /fonts/inter-v12-latin-700.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="crossorigin"
+        />
+        <link
+          rel="preload"
+          href=" /fonts/inter-v12-latin-800.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="crossorigin"
+        />
       </Head>
       <body>
         <Main />
